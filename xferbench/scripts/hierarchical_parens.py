69b21dcc66824ea5c8f2fb0213d569cb8bb37127
Brendon Boldt <bboldt@cs.cmu.edu>
2024-09-09 09:34:32 -0400

    Parameterize pz-syn generation

--- xferbench/scripts/hierarchical_parens.py
+++ xferbench/scripts/hierarchical_parens.py
@@ -1,16 +1,15 @@
 # Developed from: https://github.com/toizzy/tilt-transfer/blob/master/corpora/constructed_corpora/hierarchical_parens.py
 
 from collections import deque
+import sys
 
 import numpy as np
 import datasets  # type: ignore
-
 
 open_prob = 0.4
 
 N_TOKENS = 100_000_000
 CONTEXT_SIZE = 256
-data = np.empty((N_TOKENS // CONTEXT_SIZE + 1) * CONTEXT_SIZE, dtype=np.int32)
 # Account for potential special tokens
 VOCAB_SIZE = 30_000 - 10
 
@@ -18,25 +17,37 @@ VOCAB_SIZE = 30_000 - 10
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4176592/
 alpha = 1
 beta = 2.7
-ps = (np.arange(VOCAB_SIZE) + 1 + beta) ** -alpha
-ps /= sum(ps)
 
-# Note: The seed was added after the dataset used in the paper was generated,
-# so exact reproduction is not gauranteed.
-rng = np.random.default_rng(0)
+def main() -> None:
+    data = np.empty((N_TOKENS // CONTEXT_SIZE + 1) * CONTEXT_SIZE, dtype=np.int32)
 
-open_deque: deque[int] = deque()
-open_decision = rng.choice([0, 1], len(data))
-samples = rng.choice(VOCAB_SIZE, len(data), p=ps)
-for i in range(len(data)):
-    if open_decision[i] or len(open_deque) == 0:
-        data[i] = samples[i]
-        open_deque.append(data[i])
+    ps = (np.arange(VOCAB_SIZE) + 1 + beta) ** -alpha
+    ps /= sum(ps)
+
+    if len(sys.argv) >= 2:
+        seed = int(sys.argv[1])
     else:
-        last_open = open_deque.pop()
-        data[i] = last_open
+        seed = 0
 
-data = data.reshape(-1, CONTEXT_SIZE)
+    # Note: The seed was added after the dataset used in the paper was generated,
+    # so exact reproduction is not gauranteed.
+    rng = np.random.default_rng(seed)
 
-ds = datasets.Dataset.from_dict({"input_ids": data})
-ds.save_to_disk("data/baselines/pz_syn")
+    open_deque: deque[int] = deque()
+    open_decision = rng.choice([0, 1], len(data))
+    samples = rng.choice(VOCAB_SIZE, len(data), p=ps)
+    for i in range(len(data)):
+        if open_decision[i] or len(open_deque) == 0:
+            data[i] = samples[i]
+            open_deque.append(data[i])
+        else:
+            last_open = open_deque.pop()
+            data[i] = last_open
+
+    data = data.reshape(-1, CONTEXT_SIZE)
+
+    ds = datasets.Dataset.from_dict({"input_ids": data})
+    ds.save_to_disk(f"data/var/dyck-seed-{seed}")
+
+if __name__ == "__main__":
+    main()
